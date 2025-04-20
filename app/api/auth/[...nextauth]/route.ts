9ccf16c77eb1f3b3afedbe3f63e3614c8dc67795
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-21 00:27:11 +0530

    refactor: replace NextAuth implementation with custom handlers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,2 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const OPTIONS: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/sign-in"
-  }
-};
-
-export const handler = NextAuth(OPTIONS);
-
-export { handler as GET, handler as POST };
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
